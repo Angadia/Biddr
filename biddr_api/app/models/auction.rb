c9ba8db6fe38dd475c68d4ed95f7eccbbb93a1d6
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-02-18 14:00:20 -0800

    Added create, index, show actions for auctions controller; Added create action for bids controller.

---
+++ biddr_api/app/models/auction.rb
@@ -0,0 +1,24 @@
+class Auction < ApplicationRecord
+  has_many :bids, dependent: :destroy
+
+  before_validation :capitalize_title
+  
+  validates :title, presence: true 
+  validates :description, length: {minimum: 5, maximum: 500}
+  validates :title, uniqueness: {scope: :description}
+  validates :reserve_price, numericality: {greater_than_or_equal_to: 0}
+  validate :ends_at_cannot_be_in_the_past
+  
+
+  private
+
+  def ends_at_cannot_be_in_the_past
+    if ends_at.present? && ends_at < Date.today
+      errors.add(:ends_at, "can't be in the past")
+    end
+  end
+
+  def capitalize_title
+    self.title = self.title.split(' ').map(&:capitalize).join(' ')
+  end
+end
