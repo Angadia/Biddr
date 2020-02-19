04e99783db6c22a19f983662118ac1e83da151b4
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-02-18 16:29:41 -0800

    Added user authentication; Added create and destroy actions for new sessions controller; Added current action for new users controller.

--- biddr_api/app/controllers/application_controller.rb
+++ biddr_api/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  private
+  
+  def authenticate_user!
+    unless user_signed_in? 
+      render json: { error: "Please Sign In" }, status: 401
+    end
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id session[:user_id]
+  end
 end
