1a64b9783afaa93405debbcdf35ed2f6cf8f556e
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-02-18 20:00:50 -0800

    Added react client public routes pages- Welcome, AuctionIndex, and AuctionShow; Added the NavBar.

---
+++ biddr_client/src/components/WelcomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const WelcomePage = props => (
+  <div className="Page">
+    <div className="ui header">Going twice. Going once.</div>
+    <div className="ui header">Sold to Biddr!</div>
+  </div>
+);
